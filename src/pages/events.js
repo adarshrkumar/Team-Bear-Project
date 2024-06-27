ee622aa8e4898f31bbbb1fe797324b52347f1417
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2024-06-26 18:41:16 -0700

    update events.js

--- src/pages/events.js
+++ src/pages/events.js
@@ -8,28 +8,28 @@ export default [
     },
     {
         "title": "Food Drive",
-        "date": "August 1",
+        "date": "July 23",
         "time": "9:00 AM - 12:00 PM, 1:00 PM - 4:00 PM",
         "location": "Sproul Plaza",
         "description": "Collect unused food to support local shelters and food banks."
     },
     {
         "title": "Sustainability Workshop",
-        "date": "September 10",
+        "date": "July 31",
         "time": "1:00 PM - 3:00 PM",
         "location": "MLK Student Union",
         "description": "Workshop on sustainable living practices and food waste reduction."
     },
     {
         "title": "Volunteer Orientation",
-        "date": "September 20",
+        "date": "August 8",
         "time": "3:00 PM - 4:00 PM",
         "location": "Dwinelle Hall, Room 105",
         "description": "Learn about volunteer opportunities and upcoming events."
     },
     {
         "title": "Harvest Festival",
-        "date": "October 5",
+        "date": "August 16",
         "time": "1:00 PM - 5:00 PM",
         "location": "Clark Kerr Community Garden",
         "description": "Celebrate the harvest with fresh produce, tours, and activities."
